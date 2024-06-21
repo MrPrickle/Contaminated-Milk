e33490360bc823e8b371fbbb27f889422763b97c
Willliam <73481552+WillBillDillPickle@users.noreply.github.com>
2024-06-21 18:09:16 -0400

    Test #1 $

--- Contaminated_Milk.cpp
+++ Contaminated_Milk.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int N, M, D, S, p[1000], m[1000], t[1000], sickP[50], sickD[50];
+int possible[1000];
 
 int main() {
     ifstream fin("badmilk.in");
@@ -16,7 +17,27 @@ int main() {
         fin >> sickP[i] >> sickD[i];
     }
 
+    int j = 0;
+    for (int i = 0; i < D; i++) {
+        for (int j = 0; j < S; j++) {
+            if (p[i] == sickP[j] && t[i] <= sickD[j]) {
+                possible[j] = m[i];
+                j++;
+            }
+        }
+    }
     
+    int maxCount, count = 0;
+    for (int i = 0; i < (sizeof(possible) / sizeof(possible[0])); i++) {
+        for (int j = 0; j < D; j++) {
+            if (m[i] == possible[j]) {
+                count++;
+            }
+        }
+        maxCount = max(maxCount, count);
+    }
+
+    fout << maxCount;
 
 
 }
