f8db68b7f45f1308dee2b1f5b9e8c110f264227e
Willliam <73481552+WillBillDillPickle@users.noreply.github.com>
2024-06-21 16:25:38 -0400

    ads

---
+++ Contaminated_Milk.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int N, M, D, S, p[1000], m[1000], t[1000];
+
+int main() {
+    ifstream fin("badmilk.in");
+    ofstream fout("badmilk.out");
+
+    
+}
