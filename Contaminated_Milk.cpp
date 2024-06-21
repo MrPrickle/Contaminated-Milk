9c25dcaadaa8bba22dfd0fd5c7067627eff4cf9f
Willliam <73481552+WillBillDillPickle@users.noreply.github.com>
2024-06-21 16:29:45 -0400

    Progress before I drink milk lol

--- Contaminated_Milk.cpp
+++ Contaminated_Milk.cpp
@@ -1,10 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-int N, M, D, S, p[1000], m[1000], t[1000];
+int N, M, D, S, p[1000], m[1000], t[1000], sickP[50], sickD[50];
 
 int main() {
     ifstream fin("badmilk.in");
     ofstream fout("badmilk.out");
 
+    fin >> N >> M >> D >> S;
+
+    for (int i = 0; i < D; i++) {
+        fin >> p[i] >> m[i] >> t[i];
+    }
     
+    for (int i = 0; i < S; i++) {
+        fin >> sickP[i] >> sickD[i];
+    }
+
+    
+
+
 }
