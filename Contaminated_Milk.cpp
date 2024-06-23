1f4887778649749de22babed12438d34330e0cd3
Willliam <73481552+WillBillDillPickle@users.noreply.github.com>
2024-06-23 12:58:55 -0400

    Try #7 $

--- Contaminated_Milk.cpp
+++ Contaminated_Milk.cpp
@@ -1,8 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <unordered_set>
 using namespace std;
-int N, M, D, S, p[1000], m[1000], t[1000], sickP[50], sickD[50];
-int possible[50][50];
+
+// Constants for the dimensions of the static arrays
+const int MAXN = 50;
+const int MAXD = 1000;
+
+int N, M, D, S;
+int p[MAXD], m[MAXD], t[MAXD], sickP[MAXN], sickT[MAXN];
+int possible[MAXN][MAXN];
 
 bool hasNoValue(int arr[], int size, int val) {
     for (int i = 0; i < size; i++) {
@@ -13,18 +19,21 @@ bool hasNoValue(int arr[], int size, int val) {
     return true;
 }
 
-vector<int> unionOf2DArray(int arr[50][50]) {
-    std::unordered_set<int> unionSet;
+// Function to find the union of all elements in a static 2D array
+vector<int> unionOf2DArray(int arr[MAXN][MAXN]) {
+    unordered_set<int> unionSet;
 
     // Iterate through the 2D array and add elements to the set
-    for (int i = 0; i < 50; ++i) {
-        for (int j = 0; j < 50; ++j) {
-            unionSet.insert(arr[i][j]);
+    for (int i = 0; i < MAXN; ++i) {
+        for (int j = 0; j < MAXN; ++j) {
+            if (arr[i][j] != 0) { // Assuming 0 is not a valid milk ID
+                unionSet.insert(arr[i][j]);
+            }
         }
     }
 
     // Convert the set to a vector
-    std::vector<int> result(unionSet.begin(), unionSet.end());
+    vector<int> result(unionSet.begin(), unionSet.end());
     return result;
 }
 
@@ -39,35 +48,42 @@ int main() {
     }
     
     for (int i = 0; i < S; i++) {
-        fin >> sickP[i] >> sickD[i];
+        fin >> sickP[i] >> sickT[i];
     }
+
+    // Initialize possible array
+    memset(possible, 0, sizeof(possible));
 
     for (int i = 0; i < D; i++) {
         for (int j = 0; j < S; j++) {
-            if (p[i] == sickP[j] && t[i] <= sickD[j]) {
-                int k = 0;
-                if (hasNoValue(possible[j], 50, m[i])) {
-                    possible[j][k] = m[i];
-                    k++;
+            if (p[i] == sickP[j] && t[i] <= sickT[j]) {
+                if (hasNoValue(possible[j], MAXN, m[i])) {
+                    for (int k = 0; k < MAXN; ++k) {
+                        if (possible[j][k] == 0) {
+                            possible[j][k] = m[i];
+                            break;
+                        }
+                    }
                 }
             }
         }
     }
-    
+
     vector<int> newVec = unionOf2DArray(possible);
     int maxCount = 0;
-    int count = 0;
-    for (int i = 0; i < newVec.size(); i++) {
-        for (int j = 0; j < D; j++) {
-            if (m[j] == newVec[i]) {
+
+    // Iterate over the union of all possible contaminated milk types
+    for (int milk : newVec) {
+        int count = 0;
+        for (int i = 0; i < D; i++) {
+            if (m[i] == milk) {
                 count++;
             }
         }
         maxCount = max(maxCount, count);
-        count = 0;
     }
 
-    fout << count;
+    fout << maxCount << endl;
 
-
+    return 0;
 }
