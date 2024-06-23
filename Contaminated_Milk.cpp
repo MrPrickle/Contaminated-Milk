370803c9318fbb23d133c01930462b9e805005ab
Willliam <73481552+WillBillDillPickle@users.noreply.github.com>
2024-06-23 12:57:21 -0400

    Try #idk $

--- Contaminated_Milk.cpp
+++ Contaminated_Milk.cpp
@@ -1,15 +1,31 @@
 #include<bits/stdc++.h>
+#include <unordered_set>
 using namespace std;
 int N, M, D, S, p[1000], m[1000], t[1000], sickP[50], sickD[50];
-int possible[50];
+int possible[50][50];
 
 bool hasNoValue(int arr[], int size, int val) {
-    for (int i  = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) {
         if (arr[i] == val) {
             return false;
         }
     }
     return true;
+}
+
+vector<int> unionOf2DArray(int arr[50][50]) {
+    std::unordered_set<int> unionSet;
+
+    // Iterate through the 2D array and add elements to the set
+    for (int i = 0; i < 50; ++i) {
+        for (int j = 0; j < 50; ++j) {
+            unionSet.insert(arr[i][j]);
+        }
+    }
+
+    // Convert the set to a vector
+    std::vector<int> result(unionSet.begin(), unionSet.end());
+    return result;
 }
 
 int main() {
@@ -26,23 +42,24 @@ int main() {
         fin >> sickP[i] >> sickD[i];
     }
 
-    int k = 0;
     for (int i = 0; i < D; i++) {
         for (int j = 0; j < S; j++) {
             if (p[i] == sickP[j] && t[i] <= sickD[j]) {
-                if (hasNoValue(possible, 50, m[i])) {
-                    possible[k] = m[i];
+                int k = 0;
+                if (hasNoValue(possible[j], 50, m[i])) {
+                    possible[j][k] = m[i];
                     k++;
                 }
             }
         }
     }
     
+    vector<int> newVec = unionOf2DArray(possible);
     int maxCount = 0;
     int count = 0;
-    for (int i = 0; i < (sizeof(possible) / sizeof(possible[0])); i++) {
+    for (int i = 0; i < newVec.size(); i++) {
         for (int j = 0; j < D; j++) {
-            if (m[j] == possible[i]) {
+            if (m[j] == newVec[i]) {
                 count++;
             }
         }
@@ -50,7 +67,7 @@ int main() {
         count = 0;
     }
 
-    fout << maxCount;
+    fout << count;
 
 
 }
